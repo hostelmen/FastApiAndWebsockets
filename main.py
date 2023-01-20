f0f063325daf42b35f46fce63ee7444ce725a42f
Андрей Шеин <silvervolve@gmail.com>
2023-01-20 19:06:56 +0300

    fixed

--- main.py
+++ main.py
@@ -3,39 +3,8 @@ from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
-html = """
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Chat</title>
-    </head>
-    <body>
-        <h1>Chat</h1>
-        <form action="" onsubmit="sendMessage(event)">
-            <input type="text" id="messageText" autocomplete="off"/>
-            <button>Send</button>
-        </form>
-        <ul id='messages'>
-        </ul>
-        <script>
-            var ws = new WebSocket("ws://localhost:8000/ws");
-            ws.onmessage = function(event) {
-                var messages = document.getElementById('messages')
-                var message = document.createElement('li')
-                var content = document.createTextNode(event.data)
-                message.appendChild(content)
-                messages.appendChild(message)
-            };
-            function sendMessage(event) {
-                var input = document.getElementById("messageText")
-                ws.send(input.value)
-                input.value = ''
-                event.preventDefault()
-            }
-        </script>
-    </body>
-</html>
-"""
+with open('index.html', 'r') as file:
+	html = file.read()
 
 
 @app.get("/")
@@ -46,8 +15,8 @@ async def get():
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
 	await websocket.accept()
-	number = 0
+	number = 1
 	while True:
-		number += 1
 		data = await websocket.receive_text()
-		await websocket.send_text(f"{number} - {data}")
+		await websocket.send_json({"message": f'{number}. {data}'})
+		number += 1
